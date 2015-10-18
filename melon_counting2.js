c93494493471864fcb11f876808f694cebec0052
Geeksten <temozex@gmail.com>
2015-10-18 14:18:34 -0700

    Initial commit

---
+++ melon_counting2.js
@@ -0,0 +1,38 @@
+// Our customers are going to buy lots of melons!
+
+var melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas', 
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua', 
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas', 
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe', 
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen', 
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn', 
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas', 
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua', 
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba', 
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+function countMelons(){
+    
+    var melonCounts = {};
+
+    for (var i = 0; i < melonsToAdd.length; i=i+1){
+        var melon = melonsToAdd[i];
+        
+        if (melon in melonCounts){
+            melonCounts[melon] = melonCounts[melon] += 1;
+            
+        }  
+        else{
+            melonCounts[melon] = 1;;
+        }
+    }
+
+    console.log(melonCounts);
+    
+}
+
+countMelons(melonsToAdd)
+
+
+
